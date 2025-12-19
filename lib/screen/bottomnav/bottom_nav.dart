efe7b0b1cb795c7b1f34ae5a261c6c5fdc2da82f
oliulalam <mdoliulalamjoy@gmail.com>
2025-12-19 15:01:41 +0600

    Add all navigation page

---
+++ lib/screen/bottomnav/bottom_nav.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavScreen extends StatefulWidget{
+  @override
+  State<BottomNavScreen> createState() => _BottomNavScreenState();
+}
+
+class _BottomNavScreenState extends State<BottomNavScreen> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
