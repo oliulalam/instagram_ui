6f24890c1fc5e262d1167e1d84eeff9282510974
oliulalam <mdoliulalamjoy@gmail.com>
2025-12-20 02:14:26 +0600

    Add tab section

--- lib/Search/search_screen.dart
+++ lib/Search/search_screen.dart
@@ -4,6 +4,26 @@ import 'package:instagram/widgets/ui_helper.dart';
 
 class SearchScreen extends StatelessWidget {
   TextEditingController searchController = TextEditingController();
+  var arrContent = [
+    {
+      "img": ''
+    },
+    {
+      "img": ''
+    },
+    {
+      "img": ''
+    },
+    {
+      "img": ''
+    },
+    {
+      "img": ''
+    },
+    {
+      "img": ''
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -36,8 +56,112 @@ class SearchScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              SizedBox(width: 20,),
-              UiHelper.CustomImage(imgUrl: "live.png")
+              SizedBox(width: 20),
+              UiHelper.CustomImage(imgUrl: "live.png"),
+            ],
+          ),
+
+          SizedBox(height: 10),
+          Row(
+            children: [
+              SizedBox(width: 15),
+              Container(
+                height: 32,
+                width: 75,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white24),
+                  borderRadius: BorderRadius.circular(3),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    UiHelper.CustomImage(imgUrl: "ictv_Icon.png"),
+                    SizedBox(width: 5),
+                    Text(
+                      "IGTV",
+                      style: TextStyle(fontSize: 14, color: Color(0XFFF9F9F9)),
+                    ),
+                  ],
+                ),
+              ),
+              //First Container end
+              SizedBox(width: 5),
+              Container(
+                height: 32,
+                width: 75,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white24),
+                  borderRadius: BorderRadius.circular(3),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    UiHelper.CustomImage(imgUrl: 'shop.png'),
+                    SizedBox(width: 5),
+                    Text(
+                      "Shop",
+                      style: TextStyle(fontSize: 14, color: Color(0XFFF9F9F9)),
+                    ),
+                  ],
+                ),
+              ),
+              // Second Container end
+              SizedBox(width: 5),
+              Container(
+                height: 32,
+                width: 75,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white24),
+                  borderRadius: BorderRadius.circular(3),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Style",
+                      style: TextStyle(fontSize: 14, color: Color(0XFFF9F9F9)),
+                    ),
+                  ],
+                ),
+              ),
+              //Third Container end
+              SizedBox(width: 5),
+              Container(
+                height: 32,
+                width: 75,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white24),
+                  borderRadius: BorderRadius.circular(3),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Sports",
+                      style: TextStyle(fontSize: 14, color: Color(0XFFF9F9F9)),
+                    ),
+                  ],
+                ),
+              ),
+              //Forth Container end
+              SizedBox(width: 5),
+              Container(
+                height: 32,
+                width: 60,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white24),
+                  borderRadius: BorderRadius.circular(3),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Auto",
+                      style: TextStyle(fontSize: 14, color: Color(0XFFF9F9F9)),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ],
